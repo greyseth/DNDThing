3e170ee705e2941088bbe4b537bf7a439b85f65d
greyseth <anargya2gilland@gmail.com>
2023-01-25 13:09:10 +0700

    unfinished DOM Manipulation implementation

--- script.js
+++ script.js
@@ -25,14 +25,41 @@ let players = [
   },
 ];
 
+let items = [
+  {
+    name: "Flashlight",
+    desc: "This increases the chance to reach a destination safely.",
+  },
+  {
+    name: "Stick",
+    desc: "This is a weapon with a low attack chance.",
+  },
+  {
+    name: "Sword",
+    desc: "This is a weapon with a moderate attack chance.",
+  },
+];
+
 function createCards() {
   players.forEach(function (el) {
-    const itemHTML = ``;
+    let itemHTML = ``;
 
-    el.inv.forEach(function (item) {});
+    el.inv.forEach(function (item) {
+      let itemAdd = `
+      <li>
+          <div>
+            <p>${item.name}</p>
+            <input type="number" value="${item.amount}"/>
+          </div>
+          <p style="font-size: 15px">${item.desc}</p>
+        </li>
+      `;
+
+      itemHTML += itemAdd;
+    });
 
     const card = `
-    <div class="char-card">
+    <div class="char-card" id="${el.name}_card">
         <input type="text" placeholder="Character name" class="pc-name" value="${el.name}" />
         <div class="card-line"></div>
         <p class="card-header">Main info</p>
@@ -48,88 +75,55 @@ function createCards() {
         </div>
         <div>
           <p class="card-header">Inventory</p>
-          <ul class="inventory">
-            <li>
-              <div>
-                <p>Item name</p>
-                <input type="number" />
-              </div>
-              <p style="font-size: 15px">Long ass description goes here</p>
-            </li>
-            <li>
-              <div>
-                <p>Item name</p>
-                <input type="number" />
-              </div>
-              <p style="font-size: 15px">Long ass description goes here</p>
-            </li>
-            <li>
-              <div>
-                <p>Item name</p>
-                <input type="number" />
-              </div>
-              <p style="font-size: 15px">Long ass description goes here</p>
-            </li>
+          <ul class="inventory" id="${el.name}_inv">
+            ${itemHTML}
           </ul>
-          <button class="card-delete">DELETE</button>
+          <button class="card-delete" onclick="${el.name}">DELETE</button>
         </div>
       </div>
     `;
-  });
-}
-
-const testingCards = false;
-
-if (testingCards) {
-  for (let i = 0; i < 10; i++) {
-    const html = `
-        <div class="char-card">
-        <input type="text" placeholder="Character name" class="pc-name" />
-        <div class="card-line"></div>
-        <p class="card-header">Main info</p>
-        <div class="main-info">
-          <div>
-            <label for="char_num">Currency</label>
-            <input type="number" id="char_num" />
-          </div>
-          <div>
-            <label for="char_lives">Lives</label>
-            <input type="number" id="char_lives" value="10" />
-          </div>
-        </div>
-        <div>
-          <p class="card-header">Inventory</p>
-          <ul class="inventory">
-            <li>
-              <div>
-                <p>Item name</p>
-                <input type="number" />
-              </div>
-              <p style="font-size: 15px">Long ass description goes here</p>
-            </li>
-            <li>
-              <div>
-                <p>Item name</p>
-                <input type="number" />
-              </div>
-              <p style="font-size: 15px">Long ass description goes here</p>
-            </li>
-            <li>
-              <div>
-                <p>Item name</p>
-                <input type="number" />
-              </div>
-              <p style="font-size: 15px">Long ass description goes here</p>
-            </li>
-          </ul>
-          <button class="card-delete">DELETE</button>
-        </div>
-      </div>
-        `;
 
     document
       .getElementById("card-container")
-      .insertAdjacentHTML("afterbegin", html);
+      .insertAdjacentHTML("afterbegin", card);
+  });
+}
+
+createCards();
+
+function createItems() {
+  items.forEach(function (el) {
+    const itemAdd = `
+    <li>
+      <p class="item-name">${el.name}</p>
+      <p>${el.desc}</p>
+      <button onclick="giveItem(${el.name})">Give</button>
+    </li> 
+    `;
+
+    document
+      .getElementById("item-list")
+      .insertAdjacentHTML("afterbegin", itemAdd);
+  });
+}
+
+function updateItems(player) {
+  const char = players.find((f) => f.name === player);
+}
+
+createItems();
+
+function giveItem(itemName) {
+  const player = players.find(
+    (f) => f.name === document.getElementById("receiver").value
+  );
+  const item = items.find((f) => itemName);
+  const amount = document.getElementById("give-amount").value;
+
+  if (player.inv.find((f) => f.name === itemName)) {
+    player.inv.find((f) => f.name === itemName).amount += amount;
+  } else {
+    player.inv.push({ name: item.name, desc: item.desc, amount: amount });
   }
 }
 
@@ -137,3 +131,15 @@ function randomize(maxNum, id) {
   const rnd = Math.floor(Math.random() * maxNum + 1);
   document.getElementById(id).textContent = rnd;
 }
+
+function addMember() {}
+
+function removeMember(id) {}
+
+// function giveItem(itemName) {
+//   const receiver = document.getElementById('receiver').value;
+//   const amount = document.getElementById('give-amount').value;
+
+//   const sel = players.find((el) => el.name === receiver);
+//   sel.inv.push()
+// }
